17569964c576ef95d09d400cccc893888297ff4b
Kristin Proudfoot <kristinproudfoot@hotmail.com>
2019-04-03 14:48:19 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    Computerbot computer = new Computerbot();
+    Customerbot customer = new Customerbot();
+
+    
+    while (customer.customerResponse != "Goodbye!")
+    {
+      System.out.println(customer.getCustomerResponse(computer.computerResponse));
+      System.out.println(computer.getComputerResponse(customer.customerResponse));
+    }
+ 
+  }
+
+}// end of Main class
+
+
