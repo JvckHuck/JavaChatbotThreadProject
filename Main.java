1565922b7eadcf340c4509039591340752c6882d
JvckHuck <48325065+JvckHuck@users.noreply.github.com>
2019-04-04 14:21:40 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main 
+{
+  Computerbot computerbot = new Computerbot();
+  Customerbot customerbot = new Customerbot();
+
+  public static void main(String[] args) 
+  {
+    Computerbot computerbot = new Computerbot();
+    Customerbot customerbot = new Customerbot();
+
+    System.out.println("I am the Custom PC Bot. What is your name?");
+    System.out.println("Nice to meet you! What would you like to choose first?");
+    //System.out.println(statement);
+    System.out.println("What would you rather have, an SSD card or a Hard drive?");
+
+    System.out.println("Which graphics card would you like?");
+  }
+
+}// end of Main class
