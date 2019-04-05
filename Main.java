e6bec924e03886632a311e3109bc900f4b481758
Kristin Proudfoot <kristinproudfoot@hotmail.com>
2019-04-05 09:22:39 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,12 +8,17 @@ class Main
     Customerbot customer = new Customerbot();
 
     
-    while (customer.customerResponse != "Goodbye!")
+    System.out.println(computer.getGreeting());
+// MINICH SAYS - you cannot check string equality with == or !=
+// YOU MUST USE .equals method and !
+    //while (customer.customerResponse != "Goodbye!")
+    while (!customer.customerResponse.equals("Goodbye"))
     {
       System.out.println(customer.getCustomerResponse(computer.computerResponse));
       System.out.println(computer.getComputerResponse(customer.customerResponse));
     }
- 
+    
+    
   }
 
 }// end of Main class
