ec093d06cc82b49d6fbc1ed6817cefa6b46ac795
JvckHuck <48325065+JvckHuck@users.noreply.github.com>
2019-04-25 14:41:20 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,7 +19,33 @@ class Main
       System.out.println("ComputerBot: " + computer.getComputerResponse(customer.customerResponse));
     }
     
+    String NAME = "Sven";
     
+    System.out.println("Hello, I am the world's first smart vending macine. My name is " + NAME + "!");
+
+    InventoryList itemsForSale = new InventoryList();
+
+    itemsForSale.addItem("energy bar");
+    itemsForSale.displayList();
+    itemsForSale.addItem("Diet Coke");
+    itemsForSale.displayList();
+    itemsForSale.removeItem("energy bar");
+    itemsForSale.displayList();
+    System.out.println(itemsForSale.selectItem(0));
+
+    Scanner keyboard = new Scanner(System.in);
+    String userRequest = keyboard.nextLine();
+
+    if (itemsForSale.containsItem(userRequest))
+    {
+      System.out.println("yes, " + userRequest + " is available.");
+    }
+    else
+    {
+      System.out.println("no, " + userRequest + " is not available.");
+    }
+
+    System.out.println("Goodbye");
   }
 
 }// end of Main class
